c88286b6d6aba646df5b2cab165bbd1345f36493
balaji <balajier2006@gmail.com>
2025-02-17 10:30:44 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 import { Loginpage } from './routes/routes'
-
+import Home from './page/home'
 function App() {
 
   return (
     <>
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Loginpage/>}/>
       </Routes>
     </>
